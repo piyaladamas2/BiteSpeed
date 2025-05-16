8b74aabfd75ef67901af1bb2d32c7c523a8c6710
piyaladamas2 <piyal.adamas2@gmail.com>
2025-05-17 00:02:26 +0530

    creation of model done

---
+++ bitespeed/src/main/java/com/piyal/bitespeed/Entity/Contact.java
@@ -0,0 +1,30 @@
+package com.piyal.bitespeed.Entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Contact {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String phoneNumber;
+  private String email;
+  private int LinkedId;
+  private String linkPrecedence;
+  private LocalDateTime createdAT;
+  private LocalDateTime updatedAt;
+  private LocalDateTime deletedAt;
+}
